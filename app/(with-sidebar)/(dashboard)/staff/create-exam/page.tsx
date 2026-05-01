7c9db732425f63a65decabb502c34e609ec227ce
mhmdharis <codeaccuracy@gmail.com>
2026-05-01 21:57:54 +0500

    fixed build error

--- app/(with-sidebar)/(dashboard)/staff/create-exam/page.tsx
+++ app/(with-sidebar)/(dashboard)/staff/create-exam/page.tsx
@@ -1,4 +1,4 @@
-import VisitDetail from "@/app/(public)/patientDashboard/visitDetail";
+import VisitDetail from "@/app/(with-sidebar)/(public)/patientDashboard/visitDetail";
 
 export default function CreateExamPage() {
   return(
